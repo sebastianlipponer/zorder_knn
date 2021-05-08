6f68c4f30790a91ccc2a363b5c081e441ac1396d
Sebastian Lipponer <mail@sebastianlipponer.de>
2021-05-08 13:57:59 +0200

    Tests: Rework FloatXorMsb test cases

--- tests/xor_msb.cpp
+++ tests/xor_msb.cpp
@@ -23,61 +23,86 @@
 #include <zorder_knn/less.hpp>
 
 #include <gtest/gtest.h>
-#include <array>
+#include <vector>
 
 namespace
 {
 
-struct Test
+struct Test { double p, q; int xor_msb; };
+
+void
+TestXorMsb(std::vector<Test> const& tests, bool only_double = false)
 {
-    double p, q;
-    int xor_msb;
-};
+    for (auto const& t : tests)
+    {
+        EXPECT_EQ(zorder_knn::detail::FloatXorMsb(t.p, t.q), t.xor_msb);
 
-std::array<Test, 18> tests{{
-    // equal
-    {  1.0, 1.0, std::numeric_limits<int>::min() },
-    { -1.0, 1.0, std::numeric_limits<int>::min() },
-
-    // differing exp
-    {  0.5, 1.0,    0 },
-    {  0.5, 1.5,    0 },
-    {  0.5, 0.125, -1 },
-    {  1.0, 2.0,    1 },
-    {  2.0, 4.0,    2 },
-
-    // equal exp, differing sig
-    {  1.0, 1.5,              -1 },
-    {  1.0, 1.75,             -1 },
-    {  1.0, 1.875,            -1 },
-    {  1.0, 1.375,            -2 },
-    {  1.0, 1.125,            -3 },
-    {  1.0, 1.0 + 1.1921e-7, -23 },
-    {  1.0, 1.0 + 3.5763e-7, -22 },
-    {  1.0, 1.0 + 2.3842e-7, -22 },
-    {  1.0, 1.0 + 4.7684e-7, -21 },
-
-    // double
-    { 1.0, 1.0 + 2.2204460e-16, -52 },
-    { 1.0, 1.0 + 4.4408921e-16, -51 }
-}};
+        if (!only_double)
+        {
+            EXPECT_EQ(zorder_knn::detail::FloatXorMsb(
+                static_cast<float>(t.p), static_cast<float>(t.q)),
+                t.xor_msb);
+        }
+    }
+}
 
 }
 
-TEST(FloatXorMsb, log_base2_uint32)
+TEST(FloatXorMsb, Equal)
 {
-    for (std::size_t j{0}; j < tests.size() - 2; ++j)
-    {
-        auto const& i = tests[j];
-        EXPECT_EQ(zorder_knn::detail::FloatXorMsb(
-            static_cast<float>(i.p), static_cast<float>(i.q)), i.xor_msb);
-    }
- }
+    TestXorMsb({
+        { 1.0,           1.0,           std::numeric_limits<int>::min() },
+        { 42.6666641235, 42.6666641235, std::numeric_limits<int>::min() }
+    });
+}
 
-TEST(FloatXorMsb, log_base2_uint64)
+TEST(FloatXorMsb, DifferingSign)
 {
-    for (auto const& i: tests)
-    {
-        EXPECT_EQ(zorder_knn::detail::FloatXorMsb(i.p, i.q), i.xor_msb);
-    }
+    TestXorMsb({
+        { 1.0,           -1.0,           std::numeric_limits<int>::min() },
+        { 42.6666641235, -42.6666641235, std::numeric_limits<int>::min() }
+    });
+}
+
+TEST(FloatXorMsb, DifferingExponent)
+{
+    TestXorMsb({
+        { 0.5,  1.0,    0 },
+        { 0.5,  1.5,    0 },
+        { 0.5,  0.125, -1 },
+        { 0.25, 0.125, -2 },
+        { 1.0,  2.0,    1 },
+        { 2.0,  4.0,    2 }
+    });
+}
+
+TEST(FloatXorMsb, DifferingSignificand)
+{
+    TestXorMsb({
+        {  1.0,     1.5,              -1 },
+        {  1.0,     1.75,             -1 },
+        {  1.0,     1.875,            -1 },
+        {  1.0,     1.375,            -2 },
+        {  1.0,     1.125,            -3 },
+        {  1.75,    1.875,            -3 },
+        {  0.5,     0.5625,           -4 },
+        {  0.21875, 0.234375,         -6 },
+        {  16.0,    18.0,              1 },
+        {  24.0,    26.0,              1 },
+        {  28.0,    30.0,              1 },
+        {  56.0,    60.0,              2 },
+        {  112.0,   120.0,             3 },
+        {  80.0,    88.0,              3 },
+        {  160.0,   176.0,             4 },
+        {  384.0,   448.0,             6 },
+        {  1.0,     1.0 + 1.1921e-7, -23 },
+        {  1.0,     1.0 + 3.5763e-7, -22 },
+        {  1.0,     1.0 + 2.3842e-7, -22 },
+        {  1.0,     1.0 + 4.7684e-7, -21 }
+    });
+
+    TestXorMsb({
+        { 1.0, 1.0 + 2.2204460e-16, -52 },
+        { 1.0, 1.0 + 4.4408921e-16, -51 }
+    }, true);
 }
